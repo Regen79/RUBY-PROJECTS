78c10df259c972e6bfe2fa74dd2bd2a8d1c1566c
Krzysztof Murias <krzychu0897@gmail.com>
2026-04-07 10:39:09 +0200

    AKTUALIZATION

--- PROJEKT1/PROJECT1.rb
+++ PROJEKT1/PROJECT1.rb
@@ -5,17 +5,20 @@ class DataReader
   def reader_csv_or_json(file)
     if file.end_with?('.csv')
       @data = CSV.read(file, col_sep: ';', encoding: 'UTF-8', headers: true)
+      puts "==============================csv=============================="
     elsif file.end_with?('.json')
       @data = JSON.parse(File.read(file))
+      puts "==============================json============================="
     else
       puts "Incorrect file"
     end
   end
 
   def validate
-    #======READING=====
+    
     @data.each do |row|
       value = row["VALUE"].to_i
+      
 
       if value >= 0
         puts "Day #{row['DATE']}: sensor #{row['SENSOR TYPE']} show value #{row['VALUE']}"
@@ -28,4 +31,6 @@ end
 
 Reader1 = DataReader.new
 Reader1.reader_csv_or_json("DATE.csv")
+Reader1.validate
+Reader1.reader_csv_or_json("DATE.json")
 Reader1.validate
