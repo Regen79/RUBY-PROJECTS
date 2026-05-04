d15293c4dfe54d9c810c55bdf8e930cab9a0468f
Krzysztof Murias <krzychu0897@gmail.com>
2026-05-04 11:37:41 +0200

    AKTUALIZATION

--- PROJECT1/PROJECT1.rb
+++ PROJECT1/PROJECT1.rb
@@ -1,22 +1,34 @@
+require 'sqlite3'
 require 'csv'
 require 'json'
 require 'yaml'
-
+#====================================DATABASE========================
+$db = SQLite3::Database.new "SENSOR_DATA.db"
+$db.execute("
+  CREATE TABLE IF NOT EXISTS Measurements(
+    numbers INTEGER PRIMARY KEY,
+    date TEXT,
+    sensor_type TEXT,
+    value REAL
+  )")
 # ==================================CSV==============================
 class CsvReader 
   attr_accessor :data
-  def initialize
-    @data=[]
+  def initialize(file)
+    @file=file
   end
-  def read(file)
-    @data = CSV.read(file, col_sep: ';', encoding: 'UTF-8', headers: true)
+  def read
+    data = CSV.read(@file, col_sep: ';', encoding: 'UTF-8', headers: true)
     puts "==============================CSV=============================="
-    @data.each do |row|
-      value = row["VALUE"].to_i
+    data.each do |row|
+      value = row["VALUE"].to_f
       if value >= 0
-        puts "Day #{row['DATE']}: sensor #{row['SENSOR TYPE']} show value #{row['VALUE']}"
+        puts "Number: #{row["NUMBER"]}, Day #{row['DATE']}: sensor #{row['SENSOR TYPE']} show value #{value}"
+        $db.execute("INSERT INTO measurements (date, sensor_type,value ) VALUES (?, ?, ?)", 
+                    [row['DATE'], row['SENSOR TYPE'], row['VALUE']]
+                   )
       else
-        puts "Day #{row['DATE']}: negative value #{row['VALUE']}. Error read"
+        puts "Number: #{row["NUMBER"]}, Day #{row['DATE']}: negative value #{value}. Error read"
       end
     end
   end
@@ -25,18 +37,21 @@ end
 # ==================================JSON========================
 class JsonReader 
   attr_accessor :data
-  def initialize
-    @data=[]
+  def initialize(file)
+    @file=file
   end
-  def read(file)
-    @data = JSON.parse(File.read(file))
+  def read
+    data = JSON.parse(File.read(@file))
     puts "==============================JSON============================="
-    @data.each do |row|
-      value = row["VALUE"].to_i
+    data.each do |row|
+      value = row["VALUE"].to_f
       if value >= 0
-        puts "Day #{row['DATE']}: sensor #{row['SENSOR TYPE']} show value #{row['VALUE']}"
+        puts "Number: #{row["NUMBER"]}, Day #{row['DATE']}: sensor #{row['SENSOR TYPE']} show value #{value}"
+        $db.execute("INSERT INTO measurements (date, sensor_type,value ) VALUES (?, ?, ?)", 
+                    [row['DATE'], row['SENSOR TYPE'], row['VALUE']]
+                   )
       else
-        puts "Day #{row['DATE']}: negative value #{row['VALUE']}. Error read"
+        puts "Number: #{row["NUMBER"]}, Day #{row['DATE']}: negative value #{value}. Error read"
       end
     end
   end
@@ -45,18 +60,21 @@ end
 # ===================================YAML====================
 class YamlReader 
   attr_accessor :data
-  def initialize
-    @data=[]
+  def initialize(file)
+    @file=file
   end
-  def read(file)
-    @data = YAML.load_file(file)
+  def read
+    data = YAML.load_file(@file)
     puts "==============================YAML============================="
-    @data.each do |row|
-      value = row["VALUE"].to_i
+    data.each do |row|
+      value = row["VALUE"].to_f
       if value >= 0
-        puts "Day #{row['DATE']}: sensor #{row['SENSOR TYPE']} show value #{row['VALUE']}"
+        puts "Number: #{row["NUMBER"]}, Day #{row['DATE']}: sensor #{row['SENSOR TYPE']} show value #{value}"
+        $db.execute("INSERT INTO measurements (date, sensor_type,value ) VALUES (?, ?, ?)", 
+                    [row['DATE'], row['SENSOR TYPE'], row['VALUE']]
+                   )
       else
-        puts "Day #{row['DATE']}: negative value #{row['VALUE']}. Error read"
+        puts "Number: #{row["NUMBER"]}, Day #{row['DATE']}: negative value #{value}. Error read"
       end
     end
   end
@@ -66,11 +84,11 @@ end
 class DataReaderFactory
   def create_reader(file,option={}) 
     if file.end_with?('.csv')
-      return CsvReader.new
+      return CsvReader.new(file)
     elsif file.end_with?('.json')
-      return JsonReader.new
+      return JsonReader.new(file)
     elsif file.end_with?('.yaml')
-      return YamlReader.new
+      return YamlReader.new(file)
     else
       raise "Incorrect file"
     end  
@@ -88,5 +106,7 @@ end
 # =================================INSTANCE=====================
 factory = DataReaderFactory.new
 reader=factory.create_reader(file)
-reader.read(file)
+reader.read
+
+
 
